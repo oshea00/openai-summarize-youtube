f3ecd145cd866a9586a2eb2f971726f18a42e4c3
Mike OShea <oshea00@gmail.com>
2024-04-07 09:22:56 -0700

    return readme for default path

--- README.md
+++ README.md
@@ -7,28 +7,37 @@ You will need to have OPENAI_API_KEY environment variable set to run this, or cr
 
 To install dependencies, use pipenv:
 
-```
-> pipenv install
-```
+`> pipenv install`
+
 Or, use pip3 with the requirements.txt
-```
-pip3 install -r requirements.txt
-```
+
+`> pip3 install -r requirements.txt`
 
 ## Usage
 To run development server:
-```
-> python server.py
-```
 
-## API Endpoint: /summarize
+`> python server.py`
 
-### Description
-This endpoint takes two parameters:
+### API Endpoint: /summarize
+
+#### Description
+This endpoint returns a textual summary of the video and takes two parameters:
 
 - `video_url` (required): A valid YouTube URL of the video whose transcript needs to be summarized.
-
 - `prompt` (optional): A user-provided prompt that specifies how the transcript output should be treated.
+
+### API Endpoint: /transcribe
+
+#### Description
+This endpoint returns the video transcript and takes takes one parameter:
+
+- `video_url` (required): A valid YouTube URL of the video whose transcript needs to be transcribed.
+
+## Examples
+
+* [Transcribe](http://127.0.0.1:5000/transcribe?video_url=https://www.youtube.com/watch?v=1egAKCKPKCk&t=4s)
+* [Summarize](http://127.0.0.1:5000/summarize?video_url=https://www.youtube.com/watch?v=1egAKCKPKCk&t=4s)
+* [Summarize with a prompt to translate into Spanish](http://127.0.0.1:5000/summarize?video_url=https://www.youtube.com/watch?v=1egAKCKPKCk&t=4s&prompt=translate%20summary%20into%20Spanish)
 
 ## License
 
--- server.py
+++ server.py
@@ -7,9 +7,11 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from openai import OpenAI
 from urllib.parse import urlparse
+import markdown
 
 GPT_MODEL = "gpt-4-turbo-preview"
 GPT_MAX_TOKENS = 128000
+MAX_PROMPT_LENGTH = 2000
 client = OpenAI()
 
 # https://pypi.org/project/youtube-transcript-api/
@@ -31,6 +33,23 @@ def get_videoid(video_url):
         print(f"Error getting video ID from URL: {e}")
         return None
 
+def get_video_transcript(video_url):
+    parsed_url = urlparse(video_url)
+    if parsed_url.scheme and parsed_url.netloc:
+        # video_url is a valid URL format
+        # continue with the rest of the code
+        video_id = get_videoid(video_url)
+        if video_id and len(video_id) > 0:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+        else:
+            return "Please provide a valid YouTube video"
+    else:
+        return "Invalid video_url format. Please provide a valid YouTube video"
+    
+    # Extract the text from the transcript
+    text = ' '.join([t['text'] for t in transcript])
+    return text
+
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
 def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
     try:
@@ -48,27 +67,26 @@ def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MO
 
 @app.route('/')
 def index():
-    return "Usage: /summarize?video_url='YouTube video URL'"
+    # return the README.md content rendered as HTML
+    with open("README.md", "r") as f:
+        content = f.read()
+
+    html_content = markdown.markdown(content)
+    return html_content
+
+@app.route('/transcribe', methods=['GET'])
+def transcribe_video():
+    video_url = request.args.get('video_url')
+    
+    transcript = get_video_transcript(video_url)
+
+    return transcript
 
 @app.route('/summarize', methods=['GET'])
 def summarize_video():
     video_url = request.args.get('video_url')
-    
-    # Validate video_url format
-    parsed_url = urlparse(video_url)
-    if parsed_url.scheme and parsed_url.netloc:
-        # video_url is a valid URL format
-        # continue with the rest of the code
-        video_id = get_videoid(video_url)
-        if video_id and len(video_id) > 0:
-            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-        else:
-            return "Please provide a valid YouTube video"
-    else:
-        return "Invalid video_url format. Please provide a valid YouTube video"
-    
-    # Extract the text from the transcript
-    text = ' '.join([t['text'] for t in transcript])
+
+    text = get_video_transcript(video_url)
     
     # Perform summarization on the text (you can use any summarization technique here)
     messages = []
@@ -85,7 +103,6 @@ def summarize_video():
     messages.append({"role": "user", "content": text})
 
     summarized_text = summarize_text(messages, prompt)
-    
     return summarized_text
 
 def summarize_text(messages, prompt):
